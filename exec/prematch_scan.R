0a429bf89e70266feb7dde5862946e41a3d4ab6e
Alexis-vs <alexis.vanstraaten@outlook.fr>
2023-09-20 11:54:54 +0200

    Fix when matchId but no odds

--- exec/prematch_scan.R
+++ exec/prematch_scan.R
@@ -1,5 +1,7 @@
 install.packages("tidyr")
+install.packages("tidyselect")
 library(tidyr)
+library(tidyselect)
 library(dplyr)
 library(rvest)
 library(xml2)
@@ -14,7 +16,8 @@ df_prematch_sports <- lapply(sports, get_sport_df, bet_status = "PREMATCH", next
 df_prematch <- do.call("rbind", df_prematch_sports)
 
 df_prematch <- df_prematch %>%
-  tidyr::drop_na(matchId) %>%
+  tidyr::drop_na(tidyselect::any_of("matchId")) %>%
+  tidyr::drop_na(tidyselect::ends_with("OddPreMatch")) %>%
   dplyr::rowwise() %>%
   dplyr::mutate(competitorFavori   = substr(names(.)[c(8,11)][which.min(c_across(c(Competitor1_OddPreMatch, Competitor2_OddPreMatch)))],11,11),
                 competitorOutsider = substr(names(.)[c(8,11)][which.max(c_across(c(Competitor1_OddPreMatch, Competitor2_OddPreMatch)))],11,11))
